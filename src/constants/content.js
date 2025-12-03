0c6a881a2dd432e33ae29dcf5821c00478933810
Rohan Chavan <rochavan1055@gmail.com>
2025-12-03 12:12:12 +0530

    save

--- src/constants/content.js
+++ src/constants/content.js
@@ -8,6 +8,7 @@ import materialUiIcon from "/assets/icons/frontend/material-ui.svg";
 import tailwindCssIcon from "/assets/icons/frontend/tailwind-css.svg";
 
 // import pythonIcon from "/assets/icons/backend/python.svg";
+import nodeIcon from "../../assets/icons/backend/nodejs.svg";
 import javaIcon from "../../assets/icons/backend/java.svg"
 import csharpIcon from "../../assets/icons/backend/csharp.svg"
 import springIcon from '../../assets/icons/backend/spring.svg'
@@ -48,7 +49,8 @@ export const TECHNOLOGIES = {
     { icon: csharpIcon, name: "C#" },
     { icon: springIcon, name: "Spring" },
     { icon: springbootIcon, name: "SpringBoot" },
-    { icon: hibernateIcon, name: "Hibernate" }, 
+    { icon: hibernateIcon, name: "Hibernate" },
+    { icon: nodeIcon, name: "Node JS" },  
   ],
   Database: [
     { icon: mysqlIcon, name: "MySQL" },
@@ -66,11 +68,11 @@ export const TECHNOLOGIES = {
 
 export const EXPERIENCES = [
   {
-    year: "Dec 2023 - Feb 2024",
-    role: "Research and Analytics Intern",
-    company: " iGurus Consultancy Services",
-    description: `I  Conducted research and data analysis in machine learning, AI, and NLP Used data preprocessing techniques to clean and prepare data for model training. Implemented models to solve real-world problems using data science techniques..`,
-    technologies: ["Core Python", "Advance Python", "ETL Process", "ML", "NLP"  ],
+    year: "Jan 2025 - July 2025",
+    role: "Java Intern",
+    company: "Seven Mentor Pvt. Ltd",
+    description: `Completed a Full-Stack Development internship at SevenMentor Pvt. Ltd., working with Java (Spring Boot) and React.js. Built responsive UIs and REST APIs while integrating frontend and backend for real-world applications.`,
+    technologies: ["Core Jav", "Advance Java", "React", "REST API", "MySQL"  ],
   },
   {
     year: "Jan 2023 - Mar 2023",
@@ -118,7 +120,7 @@ export const PROJECTS = [
     description:
       "Built a billing system with dynamic invoice creation, automated PDF generation, and a WhatsApp integration that enables users to send invoices instantly. Includes customer/product management, tax calculation, and a responsive interface for fast billing.",
     technologies: ["Spring Boot","Hibernate","MySQL","React","REST API"],
-    githubLink: "https://github.com/adie253",
+    githubLink: "",
   },
 
   {
@@ -126,28 +128,9 @@ export const PROJECTS = [
     description:
       "A web-based system designed for vehicle service center managers to store and manage customer, vehicle, and service information efficiently. It helps track service records, appointments, and job updates in one centralized platform.",
     technologies: ["JAVA/J2EE", "JSP", "HTML", "CSS", "JavaScript", "MYSQL"],
-    githubLink: "https://github.com/adie253",
-    link:"https://adypsoe.in/adypjiet/contact.html",
+    githubLink: "",
   },
 
-   {
-    title: "JobsBoard",
-    description:
-      "Created a job listing platform using MERN stack, enabling employers to post job openings and job seekers to search and apply for positions. The platform features user authentication, job filtering, and application tracking, providing a seamless experience for both employers and candidates.",
-    technologies: ["ReactJS", "JavaScript", "MongoDB", "ExpressJS", "NodeJS"],
-    githubLink: "https://github.com/adie253",
-    link:"https://jobsborad.vercel.app/",
-  },
-
-  
-   {
-    title: "Car Showroom UI",
-    description:
-      "Designed a Supercars showroom ui in figma with modern and sleek aesthetics. The design features a responsive layout, high-quality images, and intuitive navigation to enhance user experience. It showcases various car models with detailed specifications and pricing information.",
-    technologies: ["Figma", "UI/UX Design"],
-    githubLink: "https://github.com/adie253/Cars-Showroom",
-    link:"https://www.figma.com/proto/e1sfixZRdkSTfl3SYVK1Gg/Untitled?node-id=1-23&p=f&t=Cgp0ROSxvWQpLI9y-0&scaling=contain&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A3",
-  },
 ];
 
 export const CERTIFICATIONS = [
